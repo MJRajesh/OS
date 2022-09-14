1c2347820949babdcf0650e8a82d18f412c0fd6b
Rajesh <101506138+MJRajesh@users.noreply.github.com>
2022-09-14 13:44:13 +0530

    Update bankersalgorith.c

--- bankersalgorith.c
+++ bankersalgorith.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    // P0, P1, P2, P3, P4 are the Process names here
- 
     int n, m, i, j, k;
-    n = 5; // Number of processes
-    m = 3; // Number of resources
-    int alloc[5][3] = { { 0, 1, 0 }, // P0    // Allocation Matrix
-                        { 2, 0, 0 }, // P1
-                        { 3, 0, 2 }, // P2
-                        { 2, 1, 1 }, // P3
-                        { 0, 0, 2 } }; // P4
+    n = 5; 
+    m = 3;
+    int alloc[5][3] = { { 0, 1, 0 }, 
+                        { 2, 0, 0 },
+                        { 3, 0, 2 },
+                        { 2, 1, 1 }, 
+                        { 0, 0, 2 } };
  
-    int max[5][3] = { { 7, 5, 3 }, // P0    // MAX Matrix
-                      { 3, 2, 2 }, // P1
-                      { 9, 0, 2 }, // P2
-                      { 2, 2, 2 }, // P3
-                      { 4, 3, 3 } }; // P4
- 
-    int avail[3] = { 3, 3, 2 }; // Available Resources
+    int max[5][3] = { { 7, 5, 3 }, 
+                      { 3, 2, 2 },
+                      { 9, 0, 2 },
+                      { 2, 2, 2 }, 
+                      { 4, 3, 3 } }; 
+    int avail[3] = { 3, 3, 2 };
  
     int f[n], ans[n], ind = 0;
     for (k = 0; k < n; k++) {
@@ -71,9 +68,5 @@ int main()
         printf(" P%d ->", ans[i]);
       printf(" P%d", ans[n - 1]);
     }
-     
- 
     return (0);
- 
-    // This code is contributed by Deep Baldha (CandyZack)
 }
